cc149a14df98cc8e38a918e9ec33a0c6f955d7f4
whyilem <88334605+whyilem@users.noreply.github.com>
2025-12-04 22:17:32 +0000

    test probleme de git

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nez Chrome Ancien</title>
+    <title>Nez Crome Ancien</title>
     <link rel="icon" type="image/png" href="img/foret.jpg"><!--a changer-->
     <style>
         body {
