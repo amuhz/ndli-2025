b9ab0a530a9d625bd3b17c9aa700cfd0800f5643
whyilem <88334605+whyilem@users.noreply.github.com>
2025-12-04 21:09:06 +0000

    <style>         body {             background-image: url("img/foret.jpg");             background-size: cover;             background-position: center;             background-repeat: no-repeat;             background-attachment: fixed;             display: flex;             flex-direction: column;             justify-content: center;             align-items: center;             height: 100vh;             margin: 0;         }     </style>

---
+++ page/linux_distri.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="icon" type="image/png" href="img/foret.jpg">
+    <style>
+        body {
+            background-image: url("img/foret.jpg");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+    </style>
+</head>
+<body>
+    ici ces la description des distributions linux !
+</body>
+</html>
