c41cd9f327732bf8d9ed479e5cc53b6d83024f11
whyilem <88334605+whyilem@users.noreply.github.com>
2025-12-04 20:55:56 +0000

    oui

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nez Crome Ancien</title>
-    <link rel="icon" type="image/png" href="chemin/vers/logo.png"><!--a changer-->
+    <link rel="icon" type="image/png" href="img/foret.jpg"><!--a changer-->
     <style>
         body {
             background-image: url("img/foret.jpg");
@@ -13,15 +13,15 @@
             background-repeat: no-repeat;
             background-attachment: fixed;
             display: flex;
-            flex-direction: column;     /* Met les boutons l’un sous l’autre */
-            justify-content: center;    /* Centre verticalement */
-            align-items: center;        /* Centre horizontalement */
-            height: 100vh;              /* Prend toute la hauteur de l’écran */
-            margin: 0;                  /* Enlève les marges du body */
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
         }
         .btn {
             padding: 12px 25px;
-            background: linear-gradient(135deg, #6a5af9, #8750f5);
+            background: linear-gradient(135deg, #787686ff, #9188a3ff);
             color: white;
             font-size: 18px;
             border: none;
@@ -39,7 +39,7 @@
 <body>
     <button class="btn" style="margin: 10px;">Connexion/inscription</button>
     <button class="btn">Trouve Ta Distribution !</button>
-    <button class="btn">Liste distribution</button>
+    <button class="btn" style="margin: 10px;">Liste distribution</button>
     
 </body>
 </html>
