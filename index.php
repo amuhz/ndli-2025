1e6bd6d54cffd3a7e99a0c8f3747323889b9efa5
whyilem <88334605+whyilem@users.noreply.github.com>
2025-12-04 20:39:39 +0000

    petit commit de l'index et ajout du doosier img

--- index.php
+++ index.php
@@ -5,6 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nez Crome Ancien</title>
     <link rel="icon" type="image/png" href="chemin/vers/logo.png"><!--a changer-->
+    <style>
+        body {
+        background-image: url("images/fond.jpg");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        }
+  </style>
 </head>
 <body>
     bonjour je suis un tres beau site tres leger
