7f81d2156d070ada1216b9373a040a65b1260cb3
whyilem <88334605+whyilem@users.noreply.github.com>
2025-12-04 18:50:41 +0000

    index.php added

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nez Crome Ancien</title>
+    <link rel="icon" type="image/png" href="chemin/vers/logo.png"><!--a changer-->
+</head>
+<body>
+    bonjour je suis un tres beau site tres leger
+</body>
+</html>
