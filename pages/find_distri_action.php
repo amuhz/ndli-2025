40410900c3ce55064d05833027e4d39bbbd2aeaf
whyilem <88334605+whyilem@users.noreply.github.com>
2025-12-05 03:05:42 +0000

    formulaire action

---
+++ pages/find_distri_action.php
@@ -0,0 +1,16 @@
+<?php
+function sanitize($data) {
+    return htmlspecialchars(trim($data));
+}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $name = isset($_POST['name']) ? sanitize($_POST['name']) : 'Non renseigné';
+    $os = isset($_POST['os']) ? sanitize($_POST['os']) : 'Non renseigné';
+    $activities = isset($_POST['acti']) ? $_POST['acti'] : [];
+    $activities = array_map('sanitize', $activities);
+    $pc_type = isset($_POST['pc_type']) ? sanitize($_POST['pc_type']) : 'Non renseigné';
+    $skill = isset($_POST['skill']) ? sanitize($_POST['skill']) : 'Non renseigné';
+} else {
+    die('Veuillez remplir le formulaire avant.');
+}
+?>
